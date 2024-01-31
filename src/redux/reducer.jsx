274c2b969b8df339bb59c327948f05a6ca83a6fb
mohd-faishal-07 <faishalm28@gmail.com>
2024-01-31 13:06:06 +0530

    local Storage

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -1,18 +1,22 @@
-import { ADD_TO_CART, REMOVE_TO_CART } from "./constants"
+import { ADD_TO_CART, REMOVE_TO_CART } from "./constants";
 
+const initialState = {
+    cartData: [],
+};
 
-export const cartData = (data = [], action)=>{
-    
-    switch(action.type){
+export const cartData = (state = initialState.cartData, action) => {
+    console.log("Reducer action:", action);
+
+    switch (action.type) {
         case ADD_TO_CART:
-            console.log("Add to cart ",action);
-            return [action.data, ...data]
-        
+            console.log("Add to cart ", action);
+            return [...state, action.data];
+
         case REMOVE_TO_CART:
-            const remainingItems= data.filter((item)=>item.id!==action.data)
-            return [...remainingItems]
+            const remainingItems = state.filter((item) => item.id !== action.data);
+            return [...remainingItems];
 
         default:
-            return []
+            return state;
     }
-}
+};
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 const store = configureStore({
-    reducer: rootReducer
-
+    reducer: persistedReducer,
 });
 
+export const persistor = persistStore(store);
 export default store;
